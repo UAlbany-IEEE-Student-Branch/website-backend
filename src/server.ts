1a3cce46d48958516373a28cfa495888f92dcc75
acomanzo <ansonih17@gmail.com>
2021-08-22 17:03:27 -0400

    refactoring

--- src/server.ts
+++ src/server.ts
@@ -14,10 +14,9 @@ const app = express();
 app.use(cors());
 
 app.use(logger);
-app.use(cors());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.json());
 app.use('/', indexRouter);
 app.use('/', eventRouter);
 app.use('/', scheduleRouter);
